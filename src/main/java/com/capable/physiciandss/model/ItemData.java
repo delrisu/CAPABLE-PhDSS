ab7bed37690b3997f48b33f0ed1d4750c9197132
Ruufuus <rufusikx3@gmail.com>
2020-11-04 18:43:49 +0100

    Add ItemData, ItemDataArray and Range classes

---
+++ src/main/java/com/capable/physiciandss/model/ItemData.java
@@ -0,0 +1,17 @@
+package com.capable.physiciandss.model;
+
+import java.util.ArrayList;
+
+public class ItemData {
+    private String name;
+    private String runtimeid;
+    private String caption;
+    private String description;
+    private String type;
+    private int value;
+    private String requested;
+    private String unit;
+    private ArrayList<Range> range;
+    private String defaultvalue;
+    private String dynamic;
+}
---
+++ src/main/java/com/capable/physiciandss/model/ItemDataArray.java
@@ -0,0 +1,14 @@
+package com.capable.physiciandss.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ItemDataArray {
+    private ArrayList<ItemData> itemDataArray;
+}
