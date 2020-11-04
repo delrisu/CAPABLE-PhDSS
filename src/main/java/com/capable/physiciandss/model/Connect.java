2dd8971d41dd7d1b8b8d6f0a2d70e35044e75cf9
Ruufuus <rufusikx3@gmail.com>
2020-11-04 22:45:36 +0100

    Fix typo in Connect class Add getPathway, getData and get connection methods to RequestService.java

--- src/main/java/com/capable/physiciandss/model/Connect.java
+++ src/main/java/com/capable/physiciandss/model/Connect.java
@@ -8,5 +8,5 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class Connect {
-    private String dressessionid;
+    private String dresessionid;
 }
--- src/main/java/com/capable/physiciandss/requests/RequestService.java
+++ src/main/java/com/capable/physiciandss/requests/RequestService.java
@@ -1,11 +1,14 @@
 package com.capable.physiciandss.requests;
 
+import com.capable.physiciandss.model.Connect;
 import com.capable.physiciandss.model.Enactment;
+import com.capable.physiciandss.model.ItemData;
+import com.capable.physiciandss.model.Pathway;
 import com.capable.physiciandss.utils.Constants;
-import lombok.NoArgsConstructor;
 import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @Service
 public class RequestService {
@@ -31,5 +34,59 @@ public class RequestService {
         }
     }
 
+    public Pathway[] getPathway() {
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("x-apikey", Constants.X_APIKEY);
+        HttpEntity request = new HttpEntity(headers);
+
+        ResponseEntity<Pathway[]> response = restTemplate.exchange(Constants.PRS_API_URL.concat("/Pathways"),
+                HttpMethod.GET, request, Pathway[].class);
+        if (response.getStatusCode() == HttpStatus.OK) {
+            return response.getBody();
+        } else {
+            return null;
+        }
+    }
+
+    public ItemData[] getData(String sessionId) {
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("x-apikey", Constants.X_APIKEY);
+        headers.set("x-dresessionid", sessionId);
+        HttpEntity request = new HttpEntity(headers);
+
+        ResponseEntity<ItemData[]> response = restTemplate.exchange(Constants.DRE_API_URL.concat("/Data"),
+                HttpMethod.GET, request, ItemData[].class);
+        if (response.getStatusCode() == HttpStatus.OK) {
+            return response.getBody();
+        } else {
+            return null;
+        }
+    }
+
+    public Connect getConnection(String enactmentId) {
+
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("x-apikey", Constants.X_APIKEY);
+
+        UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(Constants.DRE_API_URL.concat("/Connect"))
+                .queryParam("enactmentid", enactmentId);
+        HttpEntity request = new HttpEntity(headers);
+        ResponseEntity<Connect> response = restTemplate.exchange(builder.toUriString(),
+                HttpMethod.GET, request, Connect.class);
+        if (response.getStatusCode() == HttpStatus.OK) {
+            return response.getBody();
+        } else {
+            return null;
+        }
+    }
+
 
 }
+
+
+//        MultiValueMap<String, String> requestBody = new LinkedMultiValueMap<String, String>();
+//        requestBody.add("enactmentId",enactmentId);
+
