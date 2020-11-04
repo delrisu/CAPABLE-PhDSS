98445dab4a6c107975fb69ad9898175a1ff7697f
Ruufuus <rufusikx3@gmail.com>
2020-11-04 18:18:02 +0100

    Add Enactments and Enactment class

---
+++ src/main/java/com/capable/physiciandss/model/Enactments.java
@@ -0,0 +1,15 @@
+package com.capable.physiciandss.model;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+
+@AllArgsConstructor
+@Data
+@NoArgsConstructor
+public class Enactments {
+    private ArrayList<Enactment> enactments;
+}
