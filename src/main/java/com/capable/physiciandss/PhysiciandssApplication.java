a309b4f74743225e0efe4a76cf2f52592cd48b6f
Ruufuus <rufusikx3@gmail.com>
2020-11-14 23:30:45 +0100

    Add communication non blocking via web client

--- src/main/java/com/capable/physiciandss/PhysiciandssApplication.java
+++ src/main/java/com/capable/physiciandss/PhysiciandssApplication.java
@@ -1,10 +1,10 @@
 package com.capable.physiciandss;
 
 import com.capable.physiciandss.model.Connect;
-import com.capable.physiciandss.model.Enactment;
 import com.capable.physiciandss.requests.RequestServiceWS;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import reactor.core.publisher.Mono;
 
 import java.util.Arrays;
 
@@ -14,11 +14,20 @@ public class PhysiciandssApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(PhysiciandssApplication.class, args);
-        Enactment[] enactments = new RequestServiceWS().getEnactments();
-        System.out.println(Arrays.toString(enactments));
-        System.out.println(Arrays.toString(new RequestServiceWS().getPathway()));
-        Connect connection = new RequestServiceWS().getConnection(enactments[0].getId());
-        System.out.println(connection);
-        System.out.println(Arrays.toString(new RequestServiceWS().getData(connection.getDresessionid())));
+        RequestServiceWS RS = new RequestServiceWS();
+        RS.getEnactments().subscribe(enactment -> {
+            System.out.println(Arrays.toString(enactment));
+            Mono<Connect> connect = RS.getConnection(enactment[0].getId());
+            connect.subscribe(con -> {
+                System.out.println(con.toString());
+                RS.getData(con.getDresessionid()).subscribe(data -> System.out.println(Arrays.toString(data)));
+            });
+        });
+        RS.getPathway().subscribe(pathways -> System.out.println(Arrays.toString(pathways)));
+//        System.out.println(Arrays.toString(enactments));
+//        System.out.println(Arrays.toString(new RequestServiceWS().getPathway()));
+//        Connect connection = new RequestServiceWS().getConnection(enactments[0].getId());
+//        System.out.println(connection);
+//        System.out.println(Arrays.toString(new RequestServiceWS().getData(connection.getDresessionid())));
     }
 }
--- src/main/java/com/capable/physiciandss/requests/RequestServiceWS.java
+++ src/main/java/com/capable/physiciandss/requests/RequestServiceWS.java
@@ -4,46 +4,52 @@ package com.capable.physiciandss.requests;
 import com.capable.physiciandss.configuration.WebClientConfig;
 import com.capable.physiciandss.model.Connect;
 import com.capable.physiciandss.model.Enactment;
+import com.capable.physiciandss.model.ItemData;
 import com.capable.physiciandss.model.Pathway;
 import com.capable.physiciandss.utils.Constants;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+import java.util.logging.Logger;
 
 @Service
 public class RequestServiceWS {
 
+    private final Logger log = Logger.getLogger(getClass().getName());
     WebClient webClient;
+
 
     public RequestServiceWS() {
         ApplicationContext context = new AnnotationConfigApplicationContext(WebClientConfig.class);
         webClient = context.getBean("webClient", WebClient.class);
     }
 
-    public Enactment[] getEnactments() {
+    public Mono<Enactment[]> getEnactments() {
         return webClient.get()
                 .uri(Constants.PRS_API_URL + "/Enactments")
                 .retrieve()
-                .bodyToMono(Enactment[].class).log().block();
+                .bodyToMono(Enactment[].class);
     }
 
-    public Pathway[] getPathway() {
+    public Mono<Pathway[]> getPathway() {
         return webClient.get()
                 .uri(Constants.PRS_API_URL + "/Pathways")
                 .retrieve()
-                .bodyToMono(Pathway[].class).log().block();
+                .bodyToMono(Pathway[].class);
     }
 
-    public Pathway[] getData(String sessionId) {
+    public Mono<ItemData[]> getData(String sessionId) {
         return webClient.get()
                 .uri(Constants.DRE_API_URL + "/Data")
                 .header("x-dresessionid", sessionId)
                 .retrieve()
-                .bodyToMono(Pathway[].class).log().block();
+                .bodyToMono(ItemData[].class);
     }
 
-    public Connect getConnection(String enactmentId) {
+    public Mono<Connect> getConnection(String enactmentId) {
         return webClient.get()
                 .uri(
                         uriBuilder -> uriBuilder
@@ -51,6 +57,6 @@ public class RequestServiceWS {
                                 .queryParam("enactmentid", enactmentId)
                                 .build())
                 .retrieve()
-                .bodyToMono(Connect.class).log().block();
+                .bodyToMono(Connect.class);
     }
 }
