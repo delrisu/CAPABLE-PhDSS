ad19f8629b577d42eae0490a5ecff8bcce18679f
Robert Ciemny <emeryt360@gmail.com>
2020-11-30 18:55:35 +0100

    Fix Typo

--- src/main/java/com/capable/physiciandss/utils/Constants.java
+++ src/main/java/com/capable/physiciandss/utils/Constants.java
@@ -1,5 +1,5 @@
 package com.capable.physiciandss.utils;
 
 public class Constants {
-    public final static String HAPI_BASE_URL = "http://10.131.46.196:8080/baseR4/";
+    public static final String HAPI_BASE_URL = "http://10.131.46.196:8080/baseR4/";
 }
