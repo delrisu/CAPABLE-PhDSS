2bb427ff07bed6d0e419d91e0f4ff291be6deeb1
Ruufuus <rufusikx3@gmail.com>
2020-12-10 20:58:41 +0100

    Add GOCOM_BASE_URL to Constants.java

--- src/main/java/com/capable/physiciandss/utils/Constants.java
+++ src/main/java/com/capable/physiciandss/utils/Constants.java
@@ -3,6 +3,9 @@ package com.capable.physiciandss.utils;
 public class Constants {
     public static final String HAPI_BASE_URL = "http://10.131.46.196:8080/baseR4/";
 
+    public static final String GOCOM_BASE_URL = "localhost:9000";
+
+
     public static final String X_APIKEY = "h8MUoJLm2W8iwN5KVdpi2SxLFV4rZUoL";
     public static final String DEONTICS_BASE_URL = "https://cap-dev.deontics.com/dwe/a/capable-vc";
     public static final String DRE_API_URL = "/dreapi";
