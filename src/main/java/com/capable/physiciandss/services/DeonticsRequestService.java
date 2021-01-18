7e7ab034875fef86b547a8cb98b4978192f92bf9
Ruufuus <rufusikx3@gmail.com>
2021-01-18 17:02:23 +0100

    Add comments to DeonticsRequestService.java

--- src/main/java/com/capable/physiciandss/services/DeonticsRequestService.java
+++ src/main/java/com/capable/physiciandss/services/DeonticsRequestService.java
@@ -19,12 +19,18 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 
+/**
+ * Klasa zapewniająca komunikację z komponentem Deontics Engine
+ */
 @Service
 public class DeonticsRequestService extends RootService {
 
     WebClient webClient;
 
 
+    /**
+     * Konfiguruje webClient oraz inicjalizuje loggera
+     */
     public DeonticsRequestService() {
         log = LoggerFactory.getLogger(this.getClass().getName());
         ApplicationContext context = new AnnotationConfigApplicationContext(WebClientConfig.class);
@@ -32,6 +38,9 @@ public class DeonticsRequestService extends RootService {
         log.info("DeonticsRequestService has been created");
     }
 
+    /**
+     * @return Lista wszystkich Enactment
+     */
     public Mono<Enactment[]> getEnactments() {
         return webClient.get()
                 .uri(Constants.PRS_API_URL + "/Enactments")
@@ -41,6 +50,10 @@ public class DeonticsRequestService extends RootService {
                 .bodyToMono(Enactment[].class);
     }
 
+    /**
+     * @param patientId Identyfikator pacjenta
+     * @return Lista wszystich Enactment związanych z pacjentem o zadanym identyfikataorze
+     */
     public Mono<Enactment[]> getEnactmentsByPatientId(String patientId) {
         return webClient.get()
                 .uri(uriBuilder -> uriBuilder
@@ -53,6 +66,10 @@ public class DeonticsRequestService extends RootService {
                 .bodyToMono(Enactment[].class);
     }
 
+    /**
+     * @param enactmentId Identyfikator Enactment
+     * @return Enactment o zadanym Identyfikatorze
+     */
     public Mono<Enactment[]> getEnactmentsByEnactmentId(String enactmentId) {
         return webClient.get()
                 .uri(uriBuilder -> uriBuilder
@@ -65,6 +82,10 @@ public class DeonticsRequestService extends RootService {
                 .bodyToMono(Enactment[].class);
     }
 
+    /**
+     * @param temp Wartość logiczna określająca czy Pathway może być tymczasowy
+     * @return Listę Pathway'ów spełniających kryteria
+     */
     public Mono<Pathway[]> getPathway(boolean temp) {
         return webClient.get()
                 .uri(uriBuilder -> uriBuilder
@@ -77,6 +98,10 @@ public class DeonticsRequestService extends RootService {
                 .bodyToMono(Pathway[].class);
     }
 
+    /**
+     * @param name Nazwa Pathway'a, którego chcemy uzyskać
+     * @return Pathway o zadanej nazwie
+     */
     public Mono<Pathway[]> getPathwayByName(String name) {
         return webClient.get()
                 .uri(uriBuilder -> uriBuilder
@@ -89,6 +114,14 @@ public class DeonticsRequestService extends RootService {
                 .bodyToMono(Pathway[].class);
     }
 
+
+    /**
+     * Do użycia tej metody wymagane jest wcześniejsze połączenie się do Enactment i utworzenie sesji
+     *
+     * @param enquiryName Nazwa zadania typu pytanie, którego elementy danych chcemy odczytać
+     * @param sessionId   Identyfikator sesji
+     * @return Lista elementów danych powiązanych z określonym zadaniem
+     */
     public Mono<ItemData[]> getData(String enquiryName, String sessionId) {
         return webClient.get()
                 .uri(uriBuilder -> uriBuilder
@@ -103,6 +136,12 @@ public class DeonticsRequestService extends RootService {
                 .bodyToMono(ItemData[].class);
     }
 
+    /**
+     * Metoda służąca do połączenia się do enactment i utworzenia sesji
+     *
+     * @param enactmentId Identyfikator Enactment z którym chcemy się połączyć
+     * @return Obiekt mający między innymi Identyfikator sesji wymagany, aby używać większości end-pointów DREapi
+     */
     public Mono<Connect> getConnect(String enactmentId) {
         return webClient.get()
                 .uri(
@@ -116,6 +155,13 @@ public class DeonticsRequestService extends RootService {
                 .bodyToMono(Connect.class);
     }
 
+    /**
+     * Do użycia tej metody wymagane jest wcześniejsze połączenie się do Enactment i utworzenie sesji
+     *
+     * @param runtimeid Identyfikator zadania
+     * @param sessionId Identyfikator sesji
+     * @return Zadanie o zadanym identyfikatorze
+     */
     public Mono<PlanTask> getTask(String runtimeid, String sessionId) {
         return webClient.get()
                 .uri(
@@ -130,6 +176,13 @@ public class DeonticsRequestService extends RootService {
                 .bodyToMono(PlanTask.class);
     }
 
+    /**
+     * Do użycia tej metody wymagane jest wcześniejsze połączenie się do Enactment i utworzenie sesji
+     *
+     * @param state     Określa stan zadań
+     * @param sessionId Identyfikator sesji
+     * @return Listę zadań o określonym stanie
+     */
     public Mono<PlanTask[]> getPlanTasks(String state, String sessionId) {
         return webClient.get()
                 .uri(
@@ -148,6 +201,14 @@ public class DeonticsRequestService extends RootService {
                 .bodyToMono(PlanTask[].class);
     }
 
+    /**
+     * Do użycia tej metody wymagane jest wcześniejsze połączenie się do Enactment i utworzenie sesji
+     *
+     * @param state     Określa stan zadań
+     * @param taskName  Nazwa zadania
+     * @param sessionId Identyfikator sesji
+     * @return Listę zadań znajdujących się na niższym poziomie drzewa, niż zadanie o zadanej nazwie
+     */
     public Mono<PlanTask[]> getPlanTasksUnderTask(String state, String taskName, String sessionId) {
         return webClient.get()
                 .uri(
@@ -166,6 +227,13 @@ public class DeonticsRequestService extends RootService {
                 .bodyToMono(PlanTask[].class);
     }
 
+    /**
+     * Do użycia tej metody wymagane jest wcześniejsze połączenie się do Enactment i utworzenie sesji
+     *
+     * @param taskName  Mazwa zadania
+     * @param sessionId Identyfikator sesji
+     * @return Obiekt zawierający informację czy zadanie o zadanej nazwie jest w stanie, który pozwala na jego potwierdzenie
+     */
     public Mono<QueryConfirmTask> getQueryConfirmTask(String taskName, String sessionId) {
         return webClient.get()
                 .uri(
@@ -181,6 +249,13 @@ public class DeonticsRequestService extends RootService {
     }
 
 
+    /**
+     * Metoda tworzy Enactment z ścieżką określaną przez pathwayUri oraz z pacjentem wskazanym przez patientId
+     *
+     * @param pathwayuri uri Pathway'a znajdującego się w bazie danych Deontics
+     * @param patientid  Identyfikator pacjenta
+     * @return Obiekt zawierający informację identyfikatorze sesji oraz identyfikatorze utworzonego enactment
+     */
     public Mono<EnactOutput> postEnact(String pathwayuri, String patientid) {
         return webClient.post()
                 .uri(Constants.DRE_API_URL + "/Enact")
@@ -191,6 +266,14 @@ public class DeonticsRequestService extends RootService {
                 .bodyToMono(EnactOutput.class);
     }
 
+    /**
+     * Do użycia tej metody wymagane jest wcześniejsze połączenie się do Enactment i utworzenie sesji
+     *
+     * @param name      Nazwa elementu danych
+     * @param value     Nowa wartość elementu danych
+     * @param sessionId Identyfikator sesji
+     * @return Obiekt zawierający informację między innymi o statusie operacji
+     */
     public Mono<DataValueOutput> putDataValue(String name, String value, String sessionId) {
         return webClient.put()
                 .uri(Constants.DRE_API_URL + "/DataValue")
@@ -202,6 +285,13 @@ public class DeonticsRequestService extends RootService {
                 .bodyToMono(DataValueOutput.class);
     }
 
+    /**
+     * Do użycia tej metody wymagane jest wcześniejsze połączenie się do Enactment i utworzenie sesji
+     *
+     * @param dataItemNameValuesMap Mapa, która łączy dwójki imię elementu danych oraz jego nową wartość
+     * @param sessionId             Identyfikator sesji
+     * @return Listę zawierającą obiekty zawierające informację między innymi o statusie każdej z operacji
+     */
     public Mono<DataValueOutput[]> putDataValues(HashMap<String, String> dataItemNameValuesMap, String sessionId) {
         DataValuesBody dataValuesBody = new DataValuesBody();
         ArrayList<DataValueBody> dataValueBodyArrayList = new ArrayList<>();
@@ -222,6 +312,13 @@ public class DeonticsRequestService extends RootService {
                 .bodyToMono(DataValueOutput[].class);
     }
 
+    /**
+     * Do użycia tej metody wymagane jest wcześniejsze połączenie się do Enactment i utworzenie sesji
+     *
+     * @param name      Nazwa zadania
+     * @param sessionId Identyfikator sesji
+     * @return Obiekt zawierający informację o statusie operacji
+     */
     public Mono<ConfirmTaskOutput> putConfirmTask(String name, String sessionId) {
         return webClient.put()
                 .uri(Constants.DRE_API_URL + "/ConfirmTask")
@@ -233,6 +330,13 @@ public class DeonticsRequestService extends RootService {
                 .bodyToMono(ConfirmTaskOutput.class);
     }
 
+    /**
+     * Do użycia tej metody wymagane jest wcześniejsze połączenie się do Enactment i utworzenie sesji
+     *
+     * @param enactmentid Identyfikator Enactment
+     * @param sessionId   Identyfikator sesji
+     * @return Obiekt zawierający informację o statusie operacji
+     */
     public Mono<EnactmentDeleteOutput> putEnactmentDelete(String enactmentid, String sessionId) {
         return webClient.put()
                 .uri(
